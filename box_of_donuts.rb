97f60b92a913b63653541b3152c947bb8bfebb4a
James <jmelonid@lsoc.org>
2016-02-01 11:52:37 -0600

    finished

--- box_of_donuts.rb
+++ box_of_donuts.rb
@@ -1,19 +1,17 @@
 donuts = ["boston creme", "boston creme", "old fashioned", "old fashioned", "long john", "long john", "strawberry", "stawberry", "maple", "double chocolate cake", "double chocolate cake", "some french thing?"]
-
 #How many donuts are in this box?
 puts donuts.length
 #How can you know what the 4th donut in the box is?
 puts donuts[3]
 #How can you assign donuts in this box to classmates (think of classmates as variables)
-Daniel = [2]
+Daniel = donuts[2]
 puts Daniel
-Dean = [11]
+Dean = donuts[11]
 puts Dean
-Ned = [8]
+Ned = donuts[8]
 puts Ned
-James = [0]
+James = donuts[0]
 puts James
-
 #Find a method that lets you search the box of donuts to see if yours in there
 puts donuts.include? 'boston creme'
 #If you took a donut from this array, what method would you use to completely remove it from the array..err, box
